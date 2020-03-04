7ccb7aa86bb63d07986199fbe860068cd287d627
SiddGupta <sid1591+github@gmail.com>
2020-03-04 16:24:13 -0700

    - Refactoring

--- Problem_001/Solution.cpp
+++ Problem_001/Solution.cpp
@@ -2,24 +2,33 @@
 #include <unordered_set>
 
 using namespace std;
-bool isSumPossible(int *arr, int size, int sum);
+
+bool isSumPossible(const vector<int> &arr, const int &size, const int &sum);
+void getInput(vector<int> &arr, int &size, int &sum);
+
 void main()
 {
     int size, sum;
-    cout << "Enter the size";
+    vector<int> array;
+    getInput(array, size, sum);
+    cout << (isSumPossible(array, size, sum) ? "True" : "False");
+}
+
+void getInput(vector<int> &arr, int &size, int &sum)
+{
+    cout << "Enter the number of elements:: ";
     cin >> size;
-    int *array = new int[size];
-    cout << "Enter the array";
+    arr.reserve(size);
+    cout << "Enter the array:: ";
     for (int i = 0; i < size; i++)
     {
-        cin >> array[i];
+        cin >> arr[i];
     }
-    cout << "enter the sum";
+    cout << "Enter the sum:: ";
     cin >> sum;
-    cout << (isSumPossible(array, size, sum) ? "true" : "false");
-    delete[] array;
 }
-bool isSumPossible(int *arr, int size, int sum)
+
+bool isSumPossible(const vector<int> &arr, const int &size, const int &sum)
 {
     int temp;
     unordered_set<int> arr_set;
