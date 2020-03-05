cef14c8632b1665c7062c0dfdc677483faad5515
SiddGupta <sid1591+github@gmail.com>
2020-03-05 14:16:54 -0700

    - Minor fix

--- Problem_001/Solution.cpp
+++ Problem_001/Solution.cpp
@@ -1,5 +1,6 @@
 /*+ Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
 Bonus: Can you do this in one pass?
 +*/
 
@@ -8,32 +9,34 @@ Bonus: Can you do this in one pass?
 
 using namespace std;
 
-bool IsSumPossible(const vector<int> &viInput, const int &iSize, const int &iSum);
 void GetInput(vector<int> &viInput, int &iSize, int &iSum);
+bool IsSumPossible(const vector<int> &viInput, const int &iSize, const int &iSum);
 
 void main()
 {
     int iSize, iSum;
     vector<int> viInput;
-    getInput(viInput, iSize, iSum);
-    cout << (isSumPossible(viInput, iSize, iSum) ? "True" : "False");
+    GetInput(viInput, iSize, iSum);
+    cout << (IsSumPossible(viInput, iSize, iSum) ? "True" : "False");
 }
 
-void getInput(vector<int> &viInput, int &iSize, int &iSum)
+void GetInput(vector<int> &viInput, int &iSize, int &iSum)
 {
+    int iTemp;
     cout << "Enter the number of elements:: ";
     cin >> iSize;
     viInput.reserve(iSize);
     cout << "Enter the array:: ";
     for (int i = 0; i < iSize; i++)
     {
-        cin >> viInput[i];
+        cin >> iTemp;
+        viInput.push_back(iTemp);
     }
     cout << "Enter the sum:: ";
     cin >> iSum;
 }
 
-bool isSumPossible(const vector<int> &viInput, const int &iSize, const int &iSum)
+bool IsSumPossible(const vector<int> &viInput, const int &iSize, const int &iSum)
 {
     int iDiff;
     unordered_set<int> usiDifference;
